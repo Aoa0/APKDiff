ff6633139ea876f8d42f03a59fb2f3785aa0de61
Aoa0 <837575715@qq.com>
2023-05-30 12:53:02 +0800

    add DiffAnalysis class

--- src/main/java/edu/sjtu/gosec/apkdiff/analysis/DiffAnalysis.java
+++ src/main/java/edu/sjtu/gosec/apkdiff/analysis/DiffAnalysis.java
@@ -1,4 +1,11 @@
 package edu.sjtu.gosec.apkdiff.analysis;
 
 public class DiffAnalysis {
+    private boolean compareFields() {
+        return false;
+    }
+
+    private boolean compareMethods() {
+        return false;
+    }
 }
