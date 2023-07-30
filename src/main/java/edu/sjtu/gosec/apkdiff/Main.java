5a6d92d9e068919c20eebbb795d03a70955d2ce6
Aoa0 <837575715@qq.com>
2023-07-30 11:02:27 +0800

    fix input order bug

--- src/main/java/edu/sjtu/gosec/apkdiff/Main.java
+++ src/main/java/edu/sjtu/gosec/apkdiff/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         CLIParser cliParser = new CLIParser(args);
         if(cliParser.getAnalyseOption()== AnalyseOption.PAIR) {
-            Executor executor = new Executor(cliParser.getTargetAPK(), cliParser.getSourceAPK(), cliParser.getAndroidJAR());
+            Executor executor = new Executor(cliParser.getSourceAPK(), cliParser.getTargetAPK() cliParser.getAndroidJAR());
             executor.setOutDir(cliParser.getTargetDir());
             executor.runPairAnalyse();
         } else if (cliParser.getAnalyseOption()==AnalyseOption.DIRECTORY) {
